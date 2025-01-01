dbaddf98cef1b576ef66619abf5ca6288ec7b36e
ltrochet <ltrochet@proton.me>
2025-01-01 15:12:45 +0100

    En cours de développement

--- api/middleware/recover.go
+++ api/middleware/recover.go
@@ -6,17 +6,38 @@
 package middleware
 
 import (
+	"net/http"
+
+	"github.com/archnum/sdk.base/kv"
 	"github.com/archnum/sdk.base/logger"
+	_base "github.com/archnum/sdk.base/util"
 
 	"github.com/archnum/sdk.http/api/core"
 	"github.com/archnum/sdk.http/api/render"
+	"github.com/archnum/sdk.http/api/util"
 )
 
 func Recover(logger *logger.Logger) func(core.Handler) core.Handler {
 	return func(next core.Handler) core.Handler {
 		return core.HandlerFunc(
 			func(rr render.Renderer) error {
-				// TODO
+				defer func() {
+					if data := recover(); data != nil {
+						if data == http.ErrAbortHandler {
+							panic(data)
+						}
+
+						logger.Error( //::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+							"Request error recovered",
+							kv.String("id", util.RequestID(rr.Request())),
+							kv.Any("data", data),
+							kv.String("stack", _base.Stack(5)),
+						)
+
+						rr.ResponseWriter().WriteHeader(http.StatusInternalServerError)
+					}
+				}()
+
 				return next.Serve(rr)
 			},
 		)
